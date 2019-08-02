3bd6b3d967afa0bf1e69ee47cd04a058d5d397cf
Håvard Moås <hmo@dips.no>
2019-08-03 01:04:26 +0200

    Added Plantish logo by using Skiasharp

--- src/SkiaPlayground/SkiaPlayground/MainPage.xaml.cs
+++ src/SkiaPlayground/SkiaPlayground/MainPage.xaml.cs
@@ -1,9 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using SkiaSharp;
+using SkiaSharp.Views.Forms;
 using Xamarin.Forms;
 
 namespace SkiaPlayground
@@ -16,6 +14,28 @@ namespace SkiaPlayground
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private void OnCanvasViewPaintSurface(object sender, SKPaintSurfaceEventArgs args)
+        {
+            var info = args.Info;
+            var surface = args.Surface;
+            var canvas = surface.Canvas;
+            var width = info.Width;
+            var height = info.Height;
+
+            //These variables are defined in XAML to use hot reloading
+            float.TryParse(Resources["StrokeWidth"].ToString(), out var strokeWidth);
+            var color = Resources["Color"].ToString();
+
+            canvas.Clear();
+
+            canvas.DrawLine(0, height/2, width/2-100, height/2, new SKPaint(){Color = SKColor.Parse(color),StrokeWidth = strokeWidth, IsAntialias = true });
+            var path = new SKPath();
+            path.AddArc(new SKRect(width/2-110, height/2-110, width/2+110, height/2+110),180, 180 );
+            canvas.DrawPath(path, new SKPaint() { Color = SKColor.Parse(color), Style = SKPaintStyle.Stroke, StrokeWidth = strokeWidth, IsAntialias = true});
+            canvas.DrawLine(width / 2+100, height / 2, width, height / 2, new SKPaint() { Color = SKColor.Parse(color), StrokeWidth = strokeWidth, IsAntialias = true });
+
         }
     }
 }
